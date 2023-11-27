7e9e3e0c4dbd8298724cbc93064f207d01a0e5b2
vantears <vantears@naver.com>
2023-11-27 10:22:18 +0900

    11/27
    injection practice

--- mycoolapp/src/main/java/com/vantears/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/vantears/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.vantears.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoints for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
@@ -23,4 +37,5 @@ public class FunRestController {
     public String getDailyFortune() {
         return "Today is your lucky day!";
     }
+
 }
