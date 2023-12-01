4b61dcb70d7e45128c66854f2d8cbf5bf5d28ee4
vantears <vantears@naver.com>
2023-12-01 15:47:40 +0900

    12/1
    REST exception handler practice

---
+++ restapidemo/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,42 @@
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Courage", "Lee"));
+        theStudents.add(new Student("Coffee", "Lee"));
+        theStudents.add(new Student("Hojun", "Lee"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        if((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
+    }
+}
