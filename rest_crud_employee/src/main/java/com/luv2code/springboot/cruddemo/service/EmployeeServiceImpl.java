63f7b03953adc90e34fa85dd80e9f25ea0b869f8
vantears <vantears@naver.com>
2023-12-04 20:10:33 +0900

    12/4 Spring data JPA
    Spring data JPA

--- rest_crud_employee/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ rest_crud_employee/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.service;
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,5 +18,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
